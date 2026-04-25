4610975dd38dace42eb6da121d7fee9195545bae
VOGLOSSOU <n969601@gmail.com>
2026-04-25 19:56:48 +0100

    build réussi, out

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    // Ajouter ici les domaines externes si besoin (ex: CDN, Cloudinary)
-    remotePatterns: [],
-    // Permet les images locales sans erreur pendant le dev (placeholder manquants)
-    unoptimized: false,
+    unoptimized: true,
   },
 };
 
